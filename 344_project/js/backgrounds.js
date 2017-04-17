27ff02f40fbeeaf2d83b5037910c441fc124aa84
errpoulos <erpoulos@unca.edu>
2017-04-17 10:58:54 -0400

    Update backgrounds.js

--- 344_project/js/backgrounds.js
+++ 344_project/js/backgrounds.js
@@ -1,4 +1,5 @@
-var background = ["Background:", "bg 2"];
+var background = ["Background:", "Acolyte", "Charlatan", "Criminal","Entertainer","Folk Hero","Guild Artisan", "Hermit","Noble","Outlander","Sage","Sailor", "Soldier","Urchin","Haunted One","City Watch","Clan Crafter", "Cloistered Scholar","Courtier","Faction Agent","Far Traveler", "Inheritor","Knight of the Order","Mercenary Veteran","Urban Bounty Hunter","Uthgardt Tribe Member","Waterdhavian Noble"];
+
 
 
 
