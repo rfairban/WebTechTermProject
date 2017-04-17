699382d577a9021769cf5010c85856a5835138b2
errpoulos <erpoulos@unca.edu>
2017-04-17 11:00:49 -0400

    Update skills.js

--- 344_project/js/skills.js
+++ 344_project/js/skills.js
@@ -1,4 +1,6 @@
-var skills = ["skills:", "skills 2"];
+var skills = ["Skills:", "Acrobatics (Dex)","Animal Handling (Wis)","Arcana (Int)","Athletics (Str)","Deception (Cha)","History (Int)","Insight (Wis)","Intimidation (Cha)","Investigation (Int)","Medicine (Wis)","Nature (Int)","Perception (Wis)","Performance (Cha)","Persuasion (Cha)","Religion (Int)","Sleight of Hand (Dex)","Stealth (Dex)","Survival (Wis)"
+];
+
 
 
 
