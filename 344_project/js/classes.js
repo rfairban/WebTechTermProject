95a8c159fffa817e7e17d6d730579f537dbe3b11
errpoulos <erpoulos@unca.edu>
2017-04-17 10:59:20 -0400

    Update classes.js

--- 344_project/js/classes.js
+++ 344_project/js/classes.js
@@ -1,5 +1,5 @@
 
-var classes = ["Class:","Acolyte", "Charlatan", "Criminal","Entertainer","Folk Hero","Guild Artisan", "Hermit","Noble","Outlander","Sage","Sailor", "Soldier","Urchin","Haunted One","City Watch","Clan Crafter", "Cloistered Scholar","Courtier","Faction Agent","Far Traveler", "Inheritor","Knight of the Order","Mercenary Veteran","Urban Bounty Hunter","Uthgardt Tribe Member","Waterdhavian Noble",];
+var classes = ["Class:","Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"];
 
 
 
